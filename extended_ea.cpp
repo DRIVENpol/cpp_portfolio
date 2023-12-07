b0774133751c7c28bd9eaa1ea5fd036b42e12176
Paul <84129217+DRIVENpol@users.noreply.github.com>
2023-12-07 22:44:46 +0200

    The Extended Euclidean Algorithm

---
+++ extended_ea.cpp
@@ -0,0 +1,34 @@
+// The Extended Euclidean Algorithm is an extension of the Euclidean Algorithm, 
+// which is used to find the greatest common divisor (GCD) of two integers. In addition to finding the GCD
+#include <iostream>
+
+using namespace std;
+
+// Function to implement Extended Euclidean Algorithm
+int extendedEuclidean(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    
+    int x1, y1;
+    int gcd = extendedEuclidean(b, a % b, x1, y1);
+
+    x = y1;
+    y = x1 - (a / b) * y1;
+
+    return gcd;
+}
+
+int main() {
+    int a = 35, b = 15;
+    int x, y;
+    int gcd = extendedEuclidean(a, b, x, y);
+
+    cout << "The GCD of " << a << " and " << b << " is " << gcd << endl;
+    cout << "Coefficients: x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
+// The GCD of 35 and 15 is 5
